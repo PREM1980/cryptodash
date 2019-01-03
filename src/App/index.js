56eca83b407ed5d46000712de898194a195937c8
prem lakshmanan <prem1pre@gmail.com>
2019-01-03 11:13:55 -0500

    Settings page, localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import WelcomeMessage from './WelcomeMessage';
-import styled, {css} from 'styled-components';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
 import {AppProvider} from './AppProvider';
+import Settings from '../Settings';
 
 
 class App extends Component {
@@ -12,9 +11,9 @@ class App extends Component {
     return (
       <AppLayout>
         <AppProvider>
-          <AppBar>
-            <WelcomeMessage/>
+          <AppBar>            
           </AppBar>
+          <Settings />
         </AppProvider>
       </AppLayout>
     );
