61db85aa554972509cfdc602d99e4fe96fb1db20
prem lakshmanan <prem1pre@gmail.com>
2019-01-06 21:39:47 -0500

    Add coinspotlight

--- src/Dashboard/CoinSpotlight.js
+++ src/Dashboard/CoinSpotlight.js
@@ -1,8 +1,22 @@
-// import React from 'react';
-// import {Tile} from "../Shared/Tile";
-// import {AppContext} from "../App/AppProvider";
-// import CoinImage from "../Shared/CoinImage";
+import React from 'react';
+import styled from 'styled-components';
+import {Tile} from "../Shared/Tile";
+import {AppContext} from "../App/AppProvider";
+import CoinImage from "../Shared/CoinImage";
 
-// export default function(){
-//     return <Tile>Hello Spotlight</Tile>
-// }
+const SpotlightName = styled.h2`
+    text-align: center;
+`
+
+export default function(){
+    return (
+        <AppContext.Consumer>
+            {({currentFavorite, coinList}) => 
+                <Tile>
+                    <SpotlightName>{coinList[currentFavorite].CoinName}</SpotlightName>
+                    <CoinImage spotlight coin={coinList[currentFavorite]}/>
+                </Tile>
+            }
+        </AppContext.Consumer>
+    )
+}
